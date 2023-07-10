332b3c329b7985b7810b61e6082d44bafaa2a91d
Lívia Almeida <97627561+liviaonboard@users.noreply.github.com>
2023-07-10 10:57:00 -0300

    planejar aprimoramentos

---
+++ assets/js/simple.js
@@ -0,0 +1,4 @@
+//script de cores simples
+//criar select na página html
+//cada select redireciona para uma array com uma lista de cores
+//criar eventListener para o botão com uma função que retorna uma cor do array escolhido
