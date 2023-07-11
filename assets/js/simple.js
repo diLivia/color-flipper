5f9e4c4e62505289e85bee3fe3c8231a9c148c25
Lívia Almeida <97627561+liviaonboard@users.noreply.github.com>
2023-07-10 21:09:21 -0300

    css changes

--- assets/js/simple.js
+++ assets/js/simple.js
@@ -1,4 +1,4 @@
 //script de cores simples
-//criar select na página html
+//criar select na página html - feito
 //cada select redireciona para uma array com uma lista de cores
 //criar eventListener para o botão com uma função que retorna uma cor do array escolhido
